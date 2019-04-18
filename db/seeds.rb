f2183fbbc69b017c741b09fd90d152bdc48eb953
Yousri <laifayousri@gmail.com>
2019-04-18 17:22:32 +0200

    Modification du fichier seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-require 'date'
+
+# 20.times do
+#     City.create(name: Faker::Nation.capital_city)
+#  end
 
 # 10.times do
-#    Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: ["GENERAL", "ORL", "DENTIST"].sample, zip_code: Faker::Number.number(4))
+#    Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: ["GENERAL", "ORL", "DENTIST"].sample, zip_code: Faker::Number.number(4), city_id: Faker::Number.between(1, 20))
 #   end
 
 # 10.times do
-#     Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+#     Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: Faker::Number.between(1, 20))
 #  end
 
 # 20.times do
-#     Appointment.create(doctor: Doctor.find(Faker::Number.between(1, 10)), patient: Patient.find(Faker::Number.between(1, 10)), date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today))
+#     Appointment.create(doctor: Doctor.find(Faker::Number.between(1, 10)), patient: Patient.find(Faker::Number.between(1, 10)), date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today), city_id: Faker::Number.between(1, 20))
 # end
